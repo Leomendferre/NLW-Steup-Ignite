2797e04f5351a7453973a7fdd4f0bfeece7eea74
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2023-01-24 16:20:37 -0300

    Aula 03 - Rota de completar hábito

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import './src/lib/dayjs';
+
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 
