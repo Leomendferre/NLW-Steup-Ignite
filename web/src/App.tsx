d10e4bfed80a36bc33e44be8bd8b12d29f04d34f
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2023-01-24 10:37:08 -0300

    Aula 02 - Finalizada (Criando a primeira parte do front e do mobile)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
 
-function App() {
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header/>
+          <SummaryTable />
+      </div>
+    </div>   
   )
 }
-
-export default App
