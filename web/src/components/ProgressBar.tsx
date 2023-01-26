d402c4c5aaad4d056fd9426592a2c98b811e87ba
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2023-01-26 12:21:14 -0300

    Aula-03-Começando a crianção do popover do detalhe do dia

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  return(
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        arial-label="Progresso de hábitos compeltados nesse dia"
+        arial-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{ width: `${props.progress}%`}}
+      />
+    </div>
+  )
+}
