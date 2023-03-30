6410e43d2d121c34fdb9529dd4354405c25595db
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2023-03-30 17:42:44 -0300

    Aula05(Sincronizando os hábitos completos)

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 import dayjs from 'dayjs';
 import { HabitsList } from './HabitsList';
+import { useState } from 'react';
 
 interface HabitDayProps {
   date: Date
@@ -11,10 +12,16 @@ interface HabitDayProps {
 }
 
 export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps){
-  const completedPercentage = amount > 0 ? Math.round((defaultCompleted/ amount) * 100) : 0
+  const [completed, setCompleted] = useState(defaultCompleted)
+
+  const completedPercentage = amount > 0 ? Math.round((completed/ amount) * 100) : 0
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOffWeek = dayjs(date).format('dddd')
+
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed)
+  }
 
   return (
     <Popover.Root>
@@ -36,7 +43,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
 
           <ProgressBar progress={completedPercentage} />
 
-          <HabitsList date={date} />
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 
 interface HabitLisProps {
-  date: Date
+  date: Date;
+  onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
   completedHabits: string[]
 }
 
-export function HabitsList({ date }: HabitLisProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitLisProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
   useEffect(() => {
@@ -30,6 +31,27 @@ export function HabitsList({ date }: HabitLisProps) {
     })
   }, [])
 
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`)
+
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+
+    let completedHabits: string[] = []
+    
+    if (isHabitAlreadyCompleted){
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    })
+
+    onCompletedChanged(completedHabits.length)
+  }
+
   const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
   return (
@@ -38,6 +60,7 @@ export function HabitsList({ date }: HabitLisProps) {
         return (
           <Checkbox.Root
               key={habit.id}
+              onCheckedChange={() => handleToggleHabit(habit.id)}
               checked={habitsInfo.completedHabits.includes(habit.id)}
               disabled={isDateInPast}
               className='flex items-center gap-3 group'
